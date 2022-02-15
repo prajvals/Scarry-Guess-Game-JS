1588da52ac8f3760cee56c3ae76531a5ff03bbd6
Prajval Singh <prajvaltheking@gmail.com>
2022-02-15 10:11:29 +0530

    All functionalities developed

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 'use strict';
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-let checkButton = document.querySelector('.check');
-let message = document.querySelector('.message');
-let inputNumber;
+let score = 20;
+let highestScore = 0;
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(randomNumber);
+const checkButton = document.querySelector('.check');
+const message = document.querySelector('.message');
+const highscoreLable = document.querySelector('.highscore');
+const scoreDisplay = document.querySelector('.score');
+const againButton = document.querySelector('.again');
+const inputNumberElement = document.querySelector('.guess');
+const guessLabel = document.querySelector('.number');
 
 checkButton.addEventListener('click', function () {
-  inputNumber = Number(document.querySelector('.guess').value);
-  console.log(inputNumber);
+  let inputNumber = Number(inputNumberElement.value);
+  checkWithRandomNumber(inputNumber);
 });
 
 function checkWithRandomNumber(inputNumber) {
   if (randomNumber === inputNumber) {
     message.textContent = 'Yea you guessed it right';
+    guessLabel.textContent = randomNumber;
+    checkForHighestScore();
   } else if (randomNumber > inputNumber) {
     message.textContent = 'Input Number is too low';
+    inputNumberElement.value = null;
+    score--;
   } else {
     message.textContent = 'Input Number is too high';
+    score--;
+    inputNumberElement.value = null;
+  }
+  updateScore();
+}
+
+function checkForHighestScore() {
+  if (score > highestScore) {
+    highestScore = score;
+    highscoreLable.textContent = highestScore;
   }
 }
+function updateScore() {
+  if (score > 1) scoreDisplay.textContent = score;
+  else scoreDisplay.textContent = ' Game Over ';
+}
+
+againButton.addEventListener('click', function () {
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(randomNumber);
+  score = 20;
+  inputNumberElement.value = null;
+  scoreDisplay.textContent = '20';
+  message.textContent = 'Guess the number';
+  guessLabel.textContent = '?';
+});
